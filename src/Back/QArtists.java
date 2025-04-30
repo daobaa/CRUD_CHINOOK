97a0998623763027d839047616a5cbb812daa52a
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-04-30 20:04:35 +0200

    QueryArtist Test Version

--- src/Back/QArtists.java
+++ src/Back/QArtists.java
@@ -3,8 +3,8 @@ package Back;
 import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.SQLException;
 
 public class QArtists {
@@ -15,17 +15,27 @@ public class QArtists {
         String password = "postgres";
         // Conexión y consulta
         try(Connection connection = DriverManager.getConnection(url, usuari, password)){
-            // Crear un objeto Statement para ejecutar consultas
-            Statement statement=connection.createStatement();
-            // Ejecutar una consulta SELECT
-            String sql = "SELECT * FROM artist";
-            ResultSet resultSet = statement.executeQuery(sql);
-            // Procesar el resultado
-            while(resultSet.next()){
-                int id = resultSet.getInt("artist_id");
-                String nom = resultSet.getString("name");
-                System.out.println("ID: " + id + ", Nom: " + nom);
+            Scanner sc2 = new Scanner(System.in);
+
+            System.out.println("Introduce el nombre de un artista:");
+            String input = sc2.nextLine();
+            if(input.length() < 2){
+                System.out.println("Has d'introduir almenys 2 caràcters");
+            } else{
+                String sql = "SELECT * FROM artist WHERE name LIKE ?";
+
+                try(PreparedStatement statement = connection.prepareStatement(sql)){
+                    statement.setString(1, "%" + input + "%");
+                    ResultSet resultSet = statement.executeQuery();
+
+                    while(resultSet.next()){
+                        int id = resultSet.getInt("artist_id");
+                        String nom = resultSet.getString("name");
+                        System.out.println("ID: " + id + ", Nom: " + nom);
+                    }
+                }
             }
+            sc2.close();
         } catch(SQLException e){
             System.out.println("Error en la connexió o la consulta: " + e.getMessage());
         }
