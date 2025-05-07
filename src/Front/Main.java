1d719596f580b9863a0bb20fbda0d07697323036
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-05-07 21:27:03 +0200

    Javadoc Comment

--- src/Front/Main.java
+++ src/Front/Main.java
@@ -1,3 +1,14 @@
+/**
+ * Clase {@code Main}.
+ * 
+ * Esta clase contiene el metodo principal {@code main} que se ejecuta al
+ * iniciar el programa. Crea una instancia de la clase {@link Menu} y llama al 
+ * metodo {@link Menu#mainMenu()} para mostrar el menu principal al usuario.
+ * 
+ * @author Diego Andres Olivera Abarca
+ * @version 1.0
+ * @since 2025-05-07
+ */
 package Front;
 
 public class Main {
