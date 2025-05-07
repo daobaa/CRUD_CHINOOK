44a41a354d0ea0d5ba6c6607d705522aef020eae
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-05-07 19:17:16 +0200

    Base ModifyName Class

---
+++ src/Back/MName.java
@@ -0,0 +1,15 @@
+package Back;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class MName {
+    public static void ModifyName(){
+        
+    }
+}
